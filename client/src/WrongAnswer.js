19964c718d961378aaddd016133ae398b27e0274
eslam emam <89427236+eslamemam1@users.noreply.github.com>
2022-10-29 13:25:32 +0200

    new changes

--- client/src/WrongAnswer.js
+++ client/src/WrongAnswer.js
@@ -6,7 +6,7 @@ function WrongAnswer({exit}) {
       <div className=' flex justify-end w-full'>
        <button className=' w-10 h-10  text-3xl text-white mt-3 mr-5 cursor-pointer rounded-lg border-2 hover:bg-slate-600 ' onClick={exit}>X</button>
       </div>
-      <p className=' w-full flex justify-center justify-items-center text-3xl text-white mt-5 '>Wrong Answer</p> 
+      <p className=' w-full flex justify-center justify-items-center text-3xl text-red-500 mt-5 '>Wrong Answer</p> 
       </div>
   )
 }
